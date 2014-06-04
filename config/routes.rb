b13cf979bfe28ec15cf3c11c0c3104a96b6eaf7c
Yoshitake Okada <yokada@win-works.com>
2014-06-04 14:09:46 +0900

    Add callback_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/auth/:provider/callback', to: 'sessions#create'
   root 'root#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
