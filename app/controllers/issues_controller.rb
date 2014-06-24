477b4cfb8282da7010413a71a6e8449f4aa07823
Yoshitake Okada <yokada@win-works.com>
2014-06-24 18:21:11 +0900

    Add issues helper

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,102 +1,36 @@
 class IssuesController < ApplicationController
+  include IssuesHelper
+
   def export
-    none = "-"
     user = params[:user]
     repo = params[:repo]
 
     github = Github.new(:oauth_token => current_user.access_token, auto_pagination: true)
-# debug用
-#    github = Github.new(:oauth_token => current_user.access_token)
-#    binding.pry
-    
     issues_list_open = github.issues.list(:user => user, :repo => repo, state:'open')
     issues_list_closed = github.issues.list(:user => user, :repo => repo, state: 'closed')
-
     pull_requests_list_open = github.pull_requests.list(user:user, repo:repo, state:'open')
     pull_requests_list_closed = github.pull_requests.list(user:user, repo:repo, state:'closed')
+    labels_list = github.issues.labels.list(user: user, repo: repo)
 
-    #labels配列の作成(重複カテゴリは集約)
-    labels_list = Array.new
-    github.issues.labels.list(user: user, repo: repo).each do |label_in_repo|
-      colon_p = (label_in_repo.name + ":").index(":")
-      label_name = label_in_repo.name[0..colon_p]
-      if labels_list.find{ |label_in_array| label_in_array == label_name } == nil
-        labels_list << label_name
-      end
-    end
-    labels_list.sort!
-    labels_list.sort!{ |a, b|
-      x = a.include?(":") ? -1 : 1
-      y = b.include?(":") ? -1 : 1
-      x = x - 1 if a.include?("type") || a.include?("pri")
-      y = y - 1 if b.include?("type") || b.include?("pri")
-      x == y ? a <=> b : x <=> y
-    }
+    #labels_in_header作成
+    labels_in_header = make_labels_in_header(labels_list)
 
     #header作成
     header = { number: "#番号", title: "タイトル", assignee: "担当者", milestone: "マイルストーン", state: "Status"}
-    labels_list.each_with_index do |label, index|
+    labels_in_header.each_with_index do |label, index|
       header[:"#{"Label" + index.to_s}"] = label
     end
 
     #issues作成
     issues = Array.new
-    2.times do |i|
-      issues_list = issues_list_open
-      issues_list = issues_list_closed if i == 0 
-      
-      issues_list.each do |issue|
-        #pull_requestsに属するもの排除
-        next if (pull_requests_list_open.find{|pr| pr.number == issue.number} || pull_requests_list_closed.find{|pr| pr.number == issue.number})
-
-        labels = Array.new(labels_list.size, none)
-        labels_list.each_with_index do |label_in_list, index|
-          issue.labels.each do |label_in_issue|
-            if label_in_issue.name.include?(label_in_list)
-              labels[index] = if label_in_issue.name.include?(":")
-                                colon_p = (label_in_issue.name).index(":")
-                                labels[index] = label_in_issue.name[(colon_p + 1)..-1]
-                              else
-                                label_in_issue.name
-                              end
-              break
-            end
-          end
-        end
-        
-        tmp = { number: issue.number,
-                 title: issue.title,
-              assignee: (issue.assignee ? issue.assignee.login : none),
-             milestone: (issue.milestone ? issue.milestone.title : none),
-                 state: issue.state,
-              }
-        labels_list.each_with_index do |label, index|
-          tmp[:"#{"Label" + index.to_s}"] = labels[index]
-        end
-        
-        issues << tmp
-      end
-    end
+    issues_open = make_issues_list_in_csv(issues_list_open, pull_requests_list_open, labels_in_header)
+    issues_closed = make_issues_list_in_csv(issues_list_closed, pull_requests_list_closed, labels_in_header)
+    issues = issues_open + issues_closed
     issues.sort!{ |a, b| a[:number] <=> b[:number] }
 
     #csv作成
-    csv = ""
-    header.each_with_index do |(key, value), index|
-      csv += value
-      csv += ',' unless index == header.size - 1
-    end
-    csv += "\n"
-    
-    issues.each do |issue|
-      issue.each_with_index do |(key, value), index|
-        str = value.to_s
-        str.each_char{ |ch| if ch == '"' then ch = '"' + '"'; end }
-        csv += '"' + str + '"'
-        csv += ',' unless index == issue.size - 1
-      end
-      csv += "\n"
-    end
-    
+    csv = make_csv(header, issues)
+
     #BOM
     csv.prepend("\xef\xbb\xbf")
     
--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,72 @@
 module IssuesHelper
+  #header部分に該当するlabels_listをカテゴリ左側に優先の順で作成（カテゴリ重複なし）
+  def make_labels_in_header(labels_list)
+    labels_in_header = Array.new
+    labels_list.each do |label_in_repo|
+      colon_p = (label_in_repo.name + ":").index(":")
+      label_name = label_in_repo.name[0..colon_p]
+      if labels_in_header.find{ |label_in_array| label_in_array == label_name } == nil
+        labels_in_header << label_name
+      end
+    end
+    labels_in_header.sort!{ |a, b|
+      x = a.include?(":") ? -1 : 1
+      y = b.include?(":") ? -1 : 1
+      x = x - 1 if a.include?("type") || a.include?("pri")
+      y = y - 1 if b.include?("type") || b.include?("pri")
+      x == y ? a <=> b : x <=> y
+    }
+    return labels_in_header
+  end
+
+  #issues_list_from_github内でpull_requests_list_from_githubに属さないissueで
+  #issue_list_in_csvを作成。ただし、labelのカテゴリ重複なし
+  def make_issues_list_in_csv(issues_list_from_github, pull_requests_list_from_github, labels_in_header)
+    none = "-"
+    issues_list_in_csv = Array.new
+    issues_list_from_github.each do |issue_fg|
+      next if (pull_requests_list_from_github.size != 0 && pull_requests_list_from_github.find{|pr| pr.number == issue_fg.number})
+      tmp = { number: issue_fg.number,
+               title: issue_fg.title,
+            assignee: (issue_fg.assignee ? issue_fg.assignee.login : none),
+           milestone: (issue_fg.milestone ? issue_fg.milestone.title : none),
+               state: issue_fg.state,
+            }
+      labels_in_header.each_with_index do |label_in_header, index|
+        issue_fg.labels.each do |label_in_issue_fg|
+          if label_in_issue_fg.name.include?(label_in_header)
+            tmp[:"#{"Label" + index.to_s}"] = if label_in_issue_fg.name.include?(":")
+                                                colon_p = (label_in_issue_fg.name).index(":")
+                                                label_in_issue_fg.name[(colon_p + 1)..-1]
+                                              else
+                                                label_in_issue_fg.name
+                                              end
+            break
+          end
+        end
+      end
+      issues_list_in_csv << tmp
+    end
+    return issues_list_in_csv
+  end
+
+  #header, issuesからcsv作成
+  def make_csv(header, issues)
+    csv = ""
+    header.each_with_index do |(key, value), index|
+      csv += value
+      csv += ',' unless index == header.size - 1
+    end
+    csv += "\n"
+    issues.each do |issue|
+      issue.each_with_index do |(key, value), index|
+        str = value.to_s
+        str.each_char{ |ch| if ch == '"' then ch = '"' + '"'; end }
+        csv += '"' + str + '"'
+        csv += ',' unless index == issue.size - 1
+      end
+      csv += "\n"
+    end
+    return csv
+  end
 end
