2599685ce926900640a049d1b0e52bbe4dd43ff6
Yoshitake Okada <yokada@win-works.com>
2014-06-25 10:16:13 +0900

    Modify issues controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -5,10 +5,10 @@ class IssuesController < ApplicationController
     user = params[:user]
     repo = params[:repo]
 
-    github = Github.new(:oauth_token => current_user.access_token, auto_pagination: true)
-    issues_list_open = github.issues.list(:user => user, :repo => repo, state:'open')
-    issues_list_closed = github.issues.list(:user => user, :repo => repo, state: 'closed')
-    pull_requests_list_open = github.pull_requests.list(user:user, repo:repo, state:'open')
+    github = Github.new(oauth_token: current_user.access_token, auto_pagination: true)
+    issues_list_open   = github.issues.list(user: user, repo: repo, state:'open')
+    issues_list_closed = github.issues.list(user: user, repo: repo, state: 'closed')
+    pull_requests_list_open   = github.pull_requests.list(user:user, repo:repo, state:'open')
     pull_requests_list_closed = github.pull_requests.list(user:user, repo:repo, state:'closed')
     labels_list = github.issues.labels.list(user: user, repo: repo)
 
@@ -16,14 +16,18 @@ class IssuesController < ApplicationController
     labels_in_header = make_labels_in_header(labels_list)
 
     #header作成
-    header = { number: "#番号", title: "タイトル", assignee: "担当者", milestone: "マイルストーン", state: "Status"}
+    header = { number: "#番号",
+                title: "タイトル",
+             assignee: "担当者",
+            milestone: "マイルストーン",
+                state: "Status"}
     labels_in_header.each_with_index do |label, index|
-      header[:"#{"Label" + index.to_s}"] = label
+      header[:"#{"label" + index.to_s}"] = label
     end
 
     #issues作成
     issues = Array.new
-    issues_open = make_issues_list_in_csv(issues_list_open, pull_requests_list_open, labels_in_header)
+    issues_open   = make_issues_list_in_csv(issues_list_open,   pull_requests_list_open,   labels_in_header)
     issues_closed = make_issues_list_in_csv(issues_list_closed, pull_requests_list_closed, labels_in_header)
     issues = issues_open + issues_closed
     issues.sort!{ |a, b| a[:number] <=> b[:number] }
