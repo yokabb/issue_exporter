ebbad3cbd6e76adc0ce29c2510582e2a871143fe
Yoshitake Okada <yokada@win-works.com>
2014-07-01 14:08:12 +0900

    Add comment to controllers, helper and html

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,10 +1,13 @@
 class IssuesController < ApplicationController
   include IssuesHelper
 
+  # CSVファイルをエクスポートする(ユーザーがレポジトリを選択した場合に呼ばれる)
   def export
+    # 選択したレポジトリの名前と、その所有者の名前を取得する
     user = params[:user]
     repo = params[:repo]
 
+    # 選択したレポジトリ内のissueのリスト, pull requestのリスト, labelのリストをGithubから取得する
     github = Github.new(oauth_token: current_user.access_token, auto_pagination: true)
     issues_list_open   = github.issues.list(user: user, repo: repo, state: 'open')
     issues_list_closed = github.issues.list(user: user, repo: repo, state: 'closed')
@@ -12,8 +15,10 @@ class IssuesController < ApplicationController
     pull_requests_list_closed = github.pull_requests.list(user: user, repo: repo, state: 'closed')
     labels_list = github.issues.labels.list(user: user, repo: repo)
 
+    # ヘッダーのlabel部分の表示に対する前処理を行う
     labels_in_header = make_labels_in_header(labels_list)
 
+    # ヘッダーを作成する
     header = { number:    '#番号',
                title:     'タイトル',
                assignee:  '担当者',
@@ -24,14 +29,17 @@ class IssuesController < ApplicationController
       header[:"#{'label' + index.to_s}"] = label
     end
 
+    # 本体（issueリスト）を作成する。ただし、#番号順
     issues_open   = make_issues_list_in_csv(issues_list_open,   pull_requests_list_open,   labels_in_header)
     issues_closed = make_issues_list_in_csv(issues_list_closed, pull_requests_list_closed, labels_in_header)
     issues = issues_open + issues_closed
     issues.sort! { |a, b| a[:number] <=> b[:number] }
 
+    # ヘッダーとissueリストをCSV形式にする
     csv = make_csv(header, issues)
     csv.prepend("\xef\xbb\xbf")
 
+    # CSVファイルを出力する
     send_data csv, type: 'text/csv', disposition: 'attachment'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 class SessionsController < ApplicationController
+  # ユーザーをサインインさせ、ユーザー画面に移動させる
+  # (サインインボタンを押した場合に呼ばれる)
   def create
+    # ユーザーの情報をGithubから取得する
     auth = request.env['omniauth.auth']
     user = User.find_by(github_id: auth['uid'])
 
+    # データベースにユーザーの情報がない場合は登録する
     unless user
       user = User.new(github_id: auth['uid'], access_token: auth['credentials']['token'])
       user.save
     end
+
+    # ユーザーの情報が更新されていた場合は更新する
     if user.access_token != auth['credentials']['token']
       user.access_token = auth['credentials']['token']
       user.save
@@ -16,6 +22,8 @@ class SessionsController < ApplicationController
     redirect_to '/userpage'
   end
 
+  # ユーザーをサインアウトさせ、サインイン画面に戻す
+  # (サインアウトボタンを押した場合に呼ばれる)
   def destroy
     session.delete(:user_id)
     redirect_to root_path
