b23b245930b38fc8b062270bbec6c9b0f3d9dcfd
Yoshitake Okada <yokada@win-works.com>
2014-09-24 14:47:29 +0900

    Add name to user data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910002039) do
+ActiveRecord::Schema.define(version: 20140924043048) do
 
   create_table 'users', force: true do |t|
     t.integer  'github_id'
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140910002039) do
     t.datetime 'updated_at'
     t.boolean  'approved_terms'
     t.datetime 'approved_terms_at'
+    t.string   'name'
   end
 
 end
